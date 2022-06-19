fb3877cef11878a2f48aa67214570ec50971c478
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-06-19 11:27:06 +0000

    [update2]

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_19_075926) do
+ActiveRecord::Schema.define(version: 2022_06_19_112152) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
